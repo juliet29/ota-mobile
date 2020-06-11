ee1935b13a2c79da9ead88cebbe7cbaccf9dc077
Juliet Nwagwu Ume-Ezeoke <julietnu@Juliets-MacBook-Pro.local>
2020-06-11 00:03:21 -0500

    styles?

--- App.tsx
+++ App.tsx
@@ -5,19 +5,22 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LogIn from './src/pages/LogIn';
 import Home from './src/pages/Home'
 import CreatePost from './src/pages/CreatePost';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
+    <PaperProvider>
     <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen name="LogIn" component={LogIn} />
+      <Stack.Navigator headerMode="none">
+      <Stack.Screen name="LogIn" component={LogIn}/>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="CreatePost" component={CreatePost} />
       </Stack.Navigator>
     </NavigationContainer>
+    </PaperProvider>
   );
 }
 
---
+++ src/components/container.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import { View } from 'react-native';
+
+
+export const StyledView = styled(View)`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  }
+`;
