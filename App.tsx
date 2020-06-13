929d05606e4eaea40231ce00ce8d09edb7911e9b
Juliet Nwagwu Ume-Ezeoke <julietnu@Juliets-MacBook-Pro.local>
2020-06-13 12:12:20 -0500

    login context

--- App.tsx
+++ App.tsx
@@ -1,30 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import LogIn from './src/pages/LogIn';
-import Home from './src/pages/Home'
-import CreatePost from './src/pages/CreatePost';
-import { Provider as PaperProvider } from 'react-native-paper';
+import { Routes } from "./src/Routes";
+import { Providers } from "./src/Providers";
 
+export default Providers;
 
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
 
-export default function App() {
-  return (
-    <PaperProvider>
-    <NavigationContainer>
-      <Stack.Navigator headerMode="none">
-      <Stack.Screen name="LogIn" component={LogIn}/>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="CreatePost" component={CreatePost} />
-      </Stack.Navigator>
-    </NavigationContainer>
-    </PaperProvider>
-  );
-}
-
-
+// export default function App() {
+//   return (
+//     <PaperProvider>
+//     <NavigationContainer>
+//       <Stack.Navigator headerMode="none">
+//       <Stack.Screen name="LogIn" component={LogIn}/>
+//         <Stack.Screen name="Home" component={Home} />
+//         <Stack.Screen name="CreatePost" component={CreatePost} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//     </PaperProvider>
+//   );
+// }
 
 // const styles = StyleSheet.create({
 //   container: {
---
+++ src/Routes.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useContext, useState } from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  View,
+  Text,
+  Button,
+  AsyncStorage,
+  ActivityIndicator,
+} from "react-native";
+import { Center } from "./Center";
+import { AuthNavProps, AuthParamList } from "./AuthParamList";
+import { AuthStack } from "./AuthStack";
+import { AuthContext } from "./AuthProvider";
+
+interface RoutesProps {}
+
+export const Routes: React.FC<RoutesProps> = ({}) => {
+  const { user, login } = useContext(AuthContext);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // check if use is logged in or not
+    AsyncStorage.getItem("user")
+      .then((userString) => {
+        if (userString) {
+          // TODO: decode the userString
+          login();
+        }
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <Center>
+        <ActivityIndicator size="large" />
+      </Center>
+    );
+  }
+  return (
+    <NavigationContainer>
+      <AuthStack />
+    </NavigationContainer>
+  );
+};
