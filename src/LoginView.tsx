5f98c9d5ad93e8a71a34545cc568cb20882cd538
Juliet Nwagwu Ume-Ezeoke <julietnu@Juliets-MacBook-Pro.local>
2020-06-14 14:16:36 -0500

    build up log in

--- src/LoginView.tsx
+++ src/LoginView.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, Text } from "react-native";
 import { Center } from "./Center";
 import { TextInput, Button } from "react-native-paper";
 import styled from "styled-components/native";
+import { AuthNavProps } from "./AuthParamList";
+import { AuthContext } from "./AuthProvider";
 
 interface LoginViewProps {}
 
@@ -11,7 +13,7 @@ const Wrapper = styled.View`
   flex-direction: column;
   justify-content: space-around;
   padding: 30px;
-  margin-top: 300px;
+  margin-top: 250px;
   margin-bottom: 250px;
 `;
 
@@ -20,13 +22,34 @@ const Wrapper = styled.View`
 //   border-radius: 50px;
 // `;
 
-export const LoginView: React.FC<LoginViewProps> = ({}) => {
+export const LoginView: React.FC<AuthNavProps<"Login">> = ({ navigation }) => {
+  const { login } = useContext(AuthContext);
   return (
     <Wrapper>
       <TextInput label="Email" mode="outlined" />
       <TextInput label="Password" mode="outlined" />
-      <Button mode="contained" onPress={() => console.log("Pressed")}>
+      <Button
+        mode="contained"
+        onPress={() => {
+          login();
+        }}>
         SIGN IN
+      </Button>
+
+      <Button
+        mode="outlined"
+        onPress={() => {
+          console.log("Facebook button press");
+        }}>
+        Continue with Facebook
+      </Button>
+
+      <Button
+        mode="text"
+        onPress={() => {
+          navigation.navigate("Register");
+        }}>
+        Don't have an account? Start now!
       </Button>
     </Wrapper>
   );
