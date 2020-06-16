956b9a448b7eb810e7bee3fcddbabbeedc151fcd
Juliet Nwagwu Ume-Ezeoke <julietnu@Juliets-MacBook-Pro.local>
2020-06-15 23:23:13 -0500

    react hook form simple

--- src/modules/LoginView.tsx
+++ src/modules/LoginView.tsx
@@ -1,15 +1,42 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Button, TextInput } from "react-native-paper";
 import { AuthContext } from "../AuthProvider";
 import { LineBreak, StyledColumnView, Wrapper } from "../global-ui/ReusedUI";
 import { AuthNavProps } from "../navigation/auth/AuthParamList";
+import { useForm, Controller } from "react-hook-form";
+import {
+  View,
+  Alert,
+  Text,
+  Button as Button2,
+  TextInput as TextInput2,
+} from "react-native";
+import { seedValue } from "faker";
 
 interface LoginViewProps {}
 
 export const LoginView: React.FC<AuthNavProps<"Login">> = ({ navigation }) => {
+  // coming from global state management
   const { login } = useContext(AuthContext);
+  // react hook form
+  const { register, setValue, handleSubmit, errors } = useForm();
+  // TODO: fix type
+  const onSubmit = (data: any) => {
+    Alert.alert("Form Data", JSON.stringify(data));
+  };
+
+  useEffect(() => {
+    register({ name: "test" });
+  }, [register]);
+
   return (
     <Wrapper>
+      <TextInput2
+        placeholder="just a test"
+        onChangeText={(text) => setValue("test", text, true)}
+      />
+      {errors.test && <Text>This is req</Text>}
+      <Button2 title="Submit" onPress={handleSubmit(onSubmit)} />
       <StyledColumnView>
         <TextInput label="Email" mode="outlined" />
         <TextInput label="Password" mode="outlined" />
