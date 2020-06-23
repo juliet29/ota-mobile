b38f80660dfd1f06faf3cf02a57abf1c3210a2ff
Juliet Nwagwu Ume-Ezeoke <julietnu@Juliets-MacBook-Pro.local>
2020-06-23 07:20:45 -0500

    posts look decent

--- src/modules/FeedView.tsx
+++ src/modules/FeedView.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import { Center } from "../styled-components/Center";
-import { FlatList } from "react-native-gesture-handler";
-import { Button } from "react-native-paper";
+import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
+import { Button, Card, Title, Paragraph } from "react-native-paper";
 import { HomeStackNavProps } from "../navigation/app/home/HomeParamList";
 import faker from "faker";
 import { useGetPostsQuery } from "../generated-components/apolloComponents";
 import { Text, View } from "react-native";
+import { StyledColumnView } from "../styled-components/ReusedUI";
+
+// interface ItemProps {
+//   id: string,
+//   text: string
+// }
+// function Item({ id, text, selected, onSelect}: any) {
+//   return (
+//     <TouchableOpacity
+//       onPress={() => onSelect(id)}
+//       style={[
+//         styles.item,
+//         { backgroundColor: selected ? '#6e3b6e' : '#f9c2ff' },
+//       ]}
+//     >
+//       <Text style={styles.title}>{title}</Text>
+//     </TouchableOpacity>
+//   );
+// }
 
 export const FeedView: React.FC<HomeStackNavProps<"Feed">> = ({
   navigation,
@@ -24,10 +43,14 @@ export const FeedView: React.FC<HomeStackNavProps<"Feed">> = ({
     <FlatList
       data={data.getPosts}
       renderItem={({ item }) => (
-        <Text>
-          <Text> {item.id} </Text>
-          <Text> {item.text} </Text>
-        </Text>
+        <StyledColumnView>
+          <Card>
+            <Card.Content>
+              <Title>{item.user.username}</Title>
+              <Paragraph>{item.text}</Paragraph>
+            </Card.Content>
+          </Card>
+        </StyledColumnView>
       )}
       keyExtractor={(item) => item.id}
     />
@@ -35,3 +58,10 @@ export const FeedView: React.FC<HomeStackNavProps<"Feed">> = ({
 
   // <Text>{data.getPosts.map((k) => k.text)}</Text>;
 };
+
+{
+  /* <Text>
+<Text> {item.id} </Text>
+<Text> {item.text} </Text>
+</Text> */
+}
