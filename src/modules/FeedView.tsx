6f5386ad2aa740afaf5e581cde25dac5c5ef14b5
Juliet Nwagwu Ume-Ezeoke <julietnu@Juliets-MacBook-Pro.local>
2020-06-23 06:55:56 -0500

    more changes to feedview

--- src/modules/FeedView.tsx
+++ src/modules/FeedView.tsx
@@ -5,7 +5,7 @@ import { Button } from "react-native-paper";
 import { HomeStackNavProps } from "../navigation/app/home/HomeParamList";
 import faker from "faker";
 import { useGetPostsQuery } from "../generated-components/apolloComponents";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 export const FeedView: React.FC<HomeStackNavProps<"Feed">> = ({
   navigation,
@@ -20,5 +20,18 @@ export const FeedView: React.FC<HomeStackNavProps<"Feed">> = ({
     return <div>Error...</div>;
   }
 
-  return <Text>{data.getPosts.map((k) => k.text)}</Text>;
+  return (
+    <FlatList
+      data={data.getPosts}
+      renderItem={({ item }) => (
+        <Text>
+          <Text> {item.id} </Text>
+          <Text> {item.text} </Text>
+        </Text>
+      )}
+      keyExtractor={(item) => item.id}
+    />
+  );
+
+  // <Text>{data.getPosts.map((k) => k.text)}</Text>;
 };
