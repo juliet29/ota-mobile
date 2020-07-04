5a113e312a3d8cdb112f81cec0af36642e642ef2
Juliet Nwagwu Ume-Ezeoke <julietnu@Juliets-MacBook-Pro.local>
2020-07-04 12:23:05 -0500

    error handling

--- src/modules/createPost/AddContentToPost.tsx
+++ src/modules/createPost/AddContentToPost.tsx
@@ -1,56 +1,72 @@
-import React from "react";
-import { FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import { FlatList, Text } from "react-native";
 import { Button, Card, Searchbar } from "react-native-paper";
-import { useSearchSpotifyQuery } from "../../generated-components/apolloComponents";
+import {
+  useSearchSpotifyQuery,
+  SearchSpotifyQuery,
+  SearchSpotifyQueryVariables,
+} from "../../generated-components/apolloComponents";
 import { useStoreState } from "../../state-management/hooks";
 import { StyledColumnView, Wrapper } from "../../styled-components/ReusedUI";
 import { useNavigation } from "@react-navigation/native";
 
 interface AddContentToPostProps {}
 
+function filterSearchResults(someData: SearchSpotifyQuery) {
+  return someData?.search?.__typename === "ArtistSearchResult"
+    ? someData?.search?.artists?.items
+    : someData?.search?.__typename === "TrackSearchResult"
+    ? someData?.search?.tracks?.items
+    : someData?.search?.__typename === "AlbumSearchResult"
+    ? someData?.search?.albums?.items
+    : null;
+}
+
 export const AddContentToPost: React.FC<AddContentToPostProps> = ({}) => {
   const navigation = useNavigation();
-  const [searchQuery, setSearchQuery] = React.useState("");
+  const [searchQuery, setSearchQuery] = useState("");
   const postType = useStoreState((state) => state.createPost.postType);
-  // TODO put in use effect
+  const refinedSearchQuery = searchQuery.length > 1 ? searchQuery : "";
+
   const { data, loading, error } = useSearchSpotifyQuery({
     variables: {
       type: postType,
-      query: searchQuery,
+      query: refinedSearchQuery,
     },
   });
+  // console.log(data);
+  // console.log(data?.search?.__typename);
 
-  console.log(data);
-  console.log(data?.search?.__typename);
-  // if (loading) {
-  //   return <Text>Loading...</Text>;
-  // }
-  // if (error || !data) {
-  //   console.log(error);
-  //   return <Text>Error..</Text>;
-  // }
+  const searchResults = data ? filterSearchResults(data) : null;
   // TODO: handle errors
-
-  // @ts-ignore
-  const onChangeSearch = (query) => setSearchQuery(query);
 
   return (
     <Wrapper style={{ backgroundColor: "white" }}>
       <StyledColumnView>
         <Searchbar
           placeholder="Search"
-          onChangeText={onChangeSearch}
+          onChangeText={(searchQuery) => setSearchQuery(searchQuery)}
           value={searchQuery}
         />
-        {/* <FlatList
-          data={data?.search?.__typename? }
-          renderItem={(results) => (
-            <Card>
-              <Button>{results.item.name}</Button>
-            </Card>
-          )}
-          keyExtractor={(result, index) => result.name + index}
-        /> */}
+        {loading ? (
+          <Card>
+            <Text>Loading</Text>
+          </Card>
+        ) : error ? (
+          <Text></Text>
+        ) : (
+          <FlatList
+            // @ts-ignore
+            //  TODO: fix types here
+            data={searchResults}
+            renderItem={(results) => (
+              <Card>
+                <Button>{results.item?.name}</Button>
+              </Card>
+            )}
+          />
+        )}
+
         <Button onPress={() => navigation.goBack()}>Dismiss</Button>
       </StyledColumnView>
     </Wrapper>
