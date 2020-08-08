696cec58986b2bcb7b8c9b80d45c3041ebf4ef2d
Juliet Nwagwu Ume-Ezeoke <julietnu@Juliets-MacBook-Pro.local>
2020-08-08 09:57:37 -0500

    top five fine grain :)

--- src/modules/user/user-top-five/types/albums/AlbumTopFiveWrapper.tsx
+++ src/modules/user/user-top-five/types/albums/AlbumTopFiveWrapper.tsx
@@ -1,70 +1,105 @@
-import React, { useState } from "react";
-import { Button, List, Title } from "react-native-paper";
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  List,
+  Title,
+  ActivityIndicator,
+  Chip,
+  Avatar,
+} from "react-native-paper";
 import {
   GetOtherUserDocument,
   TopFiveArrayInput,
   TopFiveInput,
   useUpdateUserTopFiveMutation,
+  useGetOtherUserQuery,
 } from "../../../../../generated-components/apolloComponents";
 import { StyledColumnView } from "../../../../../styled-components/ReusedUI";
 import { TypeDisplay } from "../../TypeDisplay";
 import { TopFiveArrayType, TopFiveWrapperProps } from "../../UserTopFiveView";
 import { AlbumTopFiveEdit } from "./AlbumTopFiveEdit";
 import { useStoreState } from "../../../../../state-management/hooks";
+import { Caption } from "react-native-paper";
+import { FlatList } from "react-native-gesture-handler";
+import { array } from "yup";
+import { View } from "react-native";
 
 export const AlbumTopFiveWrapper: React.FC<TopFiveWrapperProps> = ({ id }) => {
-  const userState = useStoreState((state) => state.user.user);
-  const [albumArray, setAlbumArray] = useState(Array<TopFiveArrayType>());
-  const [searchQuery, setSearchQuery] = useState("");
-  //   const [showSettings, setShowSettings] = useState(false);
-  const [updateTopFive, { data: mdata }] = useUpdateUserTopFiveMutation();
-  // console.log("mdataAlbum", mdata ? mdata.updateUserTopFive.topAlbums : null);
+  const [array, setArray] = useState(Array<TopFiveArrayType>());
+  // console.log("id", id);
 
-  const submitUpdateTopFive = async () => {
-    const dataArray: TopFiveInput[] = albumArray;
-    const data: TopFiveArrayInput = {
-      dataArray,
-      type: "album",
-    };
-    try {
-      // make the mutation
-      await updateTopFive({
-        variables: { data },
-        refetchQueries: [
-          { query: GetOtherUserDocument, variables: { data: { id } } },
-        ],
-      });
-      // clean up
-      setSearchQuery("");
-      setAlbumArray([]);
-    } catch (err) {
-      console.log("error in album top five wrapper", err);
+  const { data, loading, error } = useGetOtherUserQuery({
+    variables: {
+      id,
+    },
+  });
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+  if (error || !data) {
+    // console.log(error);
+    return <></>;
+  }
+
+  // // process the top five
+  // let topAlbums = data.getOtherUser.topAlbums;
+  // console.log(topAlbums.length);
+  // if (topAlbums.length > 5) {
+  //   console.log("g than 5 ");
+  //   let topAlbums5 = topAlbums.slice(0, 5);
+  // }
+
+  useEffect(() => {
+    console.log("data", data);
+    if (data && data.getOtherUser.topAlbums) {
+      // might want to switch to get last 5 items in list ...
+      const topAlbums = data.getOtherUser.topAlbums.slice(0, 5);
+      console.log("top albums length:", topAlbums.length);
+      if (topAlbums.length < 5) {
+        const emptyNum = 5 - topAlbums.length;
+        console.log("empty num", emptyNum);
+        const topAlbumsWithEmpty = topAlbums.concat(
+          new Array<TopFiveArrayType>(emptyNum).fill({})
+        );
+        console.log(topAlbumsWithEmpty);
+        setArray(topAlbumsWithEmpty);
+        return;
+      }
+      setArray(topAlbums);
     }
-    // setShowSettings(false);
-  };
+  }, [data]);
 
   return (
     <StyledColumnView>
-      <Title>Top Albums</Title>
-
-      <TypeDisplay
-        id={id}
-        freshData={mdata ? mdata.updateUserTopFive.topAlbums : null}
-        type={"album"}
+      <FlatList
+        data={array}
+        keyExtractor={(item, index) => index.toString() + item}
+        renderItem={({ item, index }) => (
+          <View>
+            {item.name ? (
+              <Chip
+                avatar={
+                  <Avatar.Image
+                    size={30}
+                    source={{
+                      uri: `${item.imageUrl}`,
+                    }}
+                  />
+                }
+                onClose={() => {
+                  console.log("chip closed", index);
+                  array[index] = {};
+                  setArray(Object.assign([...array], { index: {} }));
+                }}>
+                {item.name}
+              </Chip>
+            ) : (
+              <Caption>Empty Slot </Caption>
+            )}
+          </View>
+        )}
       />
-      {userState.id === id ? (
-        <List.Accordion title="Edit Top Five">
-          <AlbumTopFiveEdit
-            array={albumArray}
-            setArray={setAlbumArray}
-            searchQuery={searchQuery}
-            setSearchQuery={setSearchQuery}
-          />
-          <Button onPress={submitUpdateTopFive}>Submit</Button>
-        </List.Accordion>
-      ) : (
-        <></>
-      )}
     </StyledColumnView>
   );
 };
