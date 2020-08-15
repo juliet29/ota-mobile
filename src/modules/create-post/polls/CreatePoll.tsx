c831209ac623432e0554f79eb224d72cedc60304
Juliet Nwagwu Ume-Ezeoke <julietnu@Juliets-MacBook-Pro.local>
2020-08-14 23:05:26 -0500

    sort, view

--- src/modules/create-post/polls/CreatePoll.tsx
+++ src/modules/create-post/polls/CreatePoll.tsx
@@ -5,6 +5,7 @@ import {
   LineBreak,
 } from "../../../styled-components/ReusedUI";
 import { FlatList, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 interface CreatePollProps {}
 type PollItemType = {
@@ -20,50 +21,78 @@ export const CreatePoll: React.FC<CreatePollProps> = ({}) => {
     console.log("poll array", array);
   }, [array]);
   return (
-    <StyledColumnView>
-      <Title>Username</Title>
-      <TextInput placeholder={"Ask a question..."} />
-      <LineBreak />
-      <FlatList
-        data={array}
-        keyExtractor={(item, index) => index.toString() + item}
-        renderItem={({ item, index }) =>
-          item.enabled ? (
-            <View style={{ display: "flex", flexDirection: "row" }}>
-              <TextInput
-                placeholder={"Enter an Answer"}
-                value={item.text}
-                onChangeText={(text) =>
-                  setArray(
-                    Object.assign([...array], {
-                      [index]: { ...item, text: text },
-                    })
-                  )
-                }
-              />
-              <IconButton icon="minus" />
-            </View>
-          ) : (
-            <View style={{ display: "flex", flexDirection: "row" }}>
-              <TextInput
-                value={item.text}
-                placeholder={"Add Option"}
-                disabled={true}
-              />
-              <IconButton
-                icon="plus"
-                onPress={() => {
-                  setArray(
-                    Object.assign([...array], {
-                      [index]: { ...item, enabled: true },
-                    })
-                  );
-                }}
-              />
-            </View>
-          )
-        }
-      />
-    </StyledColumnView>
+    <SafeAreaView>
+      <StyledColumnView style={{ marginLeft: 20, marginRight: 20 }}>
+        <Title>Username</Title>
+        <TextInput placeholder={"Ask a question..."} />
+        <LineBreak />
+        <StyledColumnView>
+          <FlatList
+            data={array.sort((a, b) => +b.enabled - +a.enabled)}
+            keyExtractor={(item, index) => index.toString() + item}
+            renderItem={({ item, index }) =>
+              item.enabled ? (
+                <View
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-around",
+                  }}>
+                  <TextInput
+                    style={{ flexGrow: 2 }}
+                    placeholder={"Enter an Answer"}
+                    value={item.text}
+                    onChangeText={(text) =>
+                      setArray(
+                        Object.assign([...array], {
+                          [index]: { ...item, text: text },
+                        })
+                      )
+                    }
+                  />
+                  <IconButton
+                    style={{ flexGrow: 0.25 }}
+                    icon="minus"
+                    onPress={() => {
+                      setArray(
+                        Object.assign([...array], {
+                          [index]: { ...item, enabled: false, text: "" },
+                        })
+                      );
+                    }}
+                  />
+                </View>
+              ) : (
+                <View
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-around",
+                    marginRight: 20,
+                  }}>
+                  <TextInput
+                    style={{ flexGrow: 2 }}
+                    value={item.text}
+                    placeholder={"Add Option"}
+                    disabled={true}
+                  />
+                  <IconButton
+                    style={{ flexGrow: 0.25 }}
+                    icon="plus"
+                    onPress={() => {
+                      setArray(
+                        Object.assign([...array], {
+                          [index]: { ...item, enabled: true },
+                        })
+                      );
+                    }}
+                  />
+                </View>
+              )
+            }
+          />
+        </StyledColumnView>
+      </StyledColumnView>
+    </SafeAreaView>
   );
 };
