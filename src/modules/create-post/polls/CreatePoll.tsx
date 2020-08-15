67793b040723b1df2bd64b6e7e3f5e4d8805df6f
Juliet Nwagwu Ume-Ezeoke <julietnu@Juliets-MacBook-Pro.local>
2020-08-14 23:57:00 -0500

    wrap simple poll data

--- src/modules/create-post/polls/CreatePoll.tsx
+++ src/modules/create-post/polls/CreatePoll.tsx
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { Title, TextInput, IconButton } from "react-native-paper";
+import {
+  Title,
+  TextInput,
+  IconButton,
+  Subheading,
+  Button,
+} from "react-native-paper";
 import {
   StyledColumnView,
   LineBreak,
 } from "../../../styled-components/ReusedUI";
 import { FlatList, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Picker } from "@react-native-community/picker";
+import RNPickerSelect from "react-native-picker-select";
 
 interface CreatePollProps {}
 type PollItemType = {
@@ -14,17 +22,42 @@ type PollItemType = {
 };
 
 export const CreatePoll: React.FC<CreatePollProps> = ({}) => {
+  const [err, setErr] = useState(false);
   const [array, setArray] = useState(
     Array<PollItemType>(4).fill({ text: "", enabled: false })
   );
-  useEffect(() => {
-    console.log("poll array", array);
-  }, [array]);
+  const [days, setDays] = useState(1);
+  const [question, setQuestion] = useState("");
+  //   useEffect(() => {
+  //     console.log("poll array", array);
+  //   }, [array]);
+
+  //   useEffect(() => {
+  //     console.log("days", days);
+  //   }, [days]);
+
+  const submitPoll = () => {
+    const options = array.filter((el) => el.text).map((el) => el.text);
+    if (options.length <= 1) {
+      setErr(true);
+    }
+    const pollData = {
+      days,
+      question,
+      options,
+    };
+    console.log("pollData sample", pollData);
+  };
+
   return (
     <SafeAreaView>
       <StyledColumnView style={{ marginLeft: 20, marginRight: 20 }}>
         <Title>Username</Title>
-        <TextInput placeholder={"Ask a question..."} />
+        <TextInput
+          placeholder={"Ask a question..."}
+          value={question}
+          onChangeText={(text) => setQuestion(text)}
+        />
         <LineBreak />
         <StyledColumnView>
           <FlatList
@@ -92,6 +125,30 @@ export const CreatePoll: React.FC<CreatePollProps> = ({}) => {
             }
           />
         </StyledColumnView>
+        <Subheading>Poll Length</Subheading>
+        {/* <RNPickerSelect
+          onValueChange={(value) => console.log(value)}
+          // items={[
+          //     { label: 'Football', value: 'football' },
+          //     { label: 'Baseball', value: 'baseball' },
+          //     { label: 'Hockey', value: 'hockey' },
+          // ]}
+          items={[
+            { label: "1 day", value: 1 },
+            { label: "2 days", value: 2 },
+            { label: "3 days", value: 3 },
+          ]}
+        /> */}
+        <Picker
+          selectedValue={days}
+          style={{ height: 50, width: 100 }}
+          onValueChange={(itemValue, itemIndex) => setDays(+itemValue)}>
+          <Picker.Item label="1 day" value={1} />
+          <Picker.Item label="2 days" value={2} />
+          <Picker.Item label="3 days" value={3} />
+        </Picker>
+
+        <Button onPress={() => submitPoll()}>Submit</Button>
       </StyledColumnView>
     </SafeAreaView>
   );
