0d007a162a477a46751c49f664ab4dd6aa0ecd46
Juliet Nwagwu Ume-Ezeoke <julietnu@Juliets-MacBook-Pro.local>
2020-08-15 01:50:49 -0500

    create playlist ok

--- src/modules/create-post/playlists/CreatePlaylist.tsx
+++ src/modules/create-post/playlists/CreatePlaylist.tsx
@@ -1,7 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { Title, TextInput, Searchbar, Caption } from "react-native-paper";
+import {
+  Title,
+  TextInput,
+  Searchbar,
+  Caption,
+  Avatar,
+  List,
+  Subheading,
+  IconButton,
+  Button,
+} from "react-native-paper";
 import { StyledColumnView } from "../../../styled-components/ReusedUI";
 import { TrackSearchPlaylist } from "./TrackSearchPlaylist";
+import { FlatList } from "react-native-gesture-handler";
 
 interface CreatePlaylistProps {}
 export type PlaylistItemType = {
@@ -17,12 +28,21 @@ export const CreatePlaylist: React.FC<CreatePlaylistProps> = ({}) => {
   const [description, setDescription] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
   const [array, setArray] = useState(Array<PlaylistItemType>().fill({}));
-  useEffect(() => {
-    console.log("update array", array);
-  }, [array]);
+  //   useEffect(() => {
+  //     console.log("update array", array);
+  //   }, [array]);
+  const submitPlaylist = () => {
+    const playlistData = {
+      title,
+      description,
+      array,
+    };
+    console.log(" playlist published", playlistData);
+  };
   return (
     <StyledColumnView style={{ marginLeft: 20, marginRight: 20 }}>
       <Title>Username</Title>
+      <Button onPress={() => submitPlaylist()}>Publish</Button>
       <TextInput
         label="Playlist Title"
         value={title}
@@ -47,6 +67,35 @@ export const CreatePlaylist: React.FC<CreatePlaylistProps> = ({}) => {
       ) : (
         <></>
       )}
+      <Title>Seclected Songs: </Title>
+      <FlatList
+        data={array}
+        keyExtractor={(item, index) => item!?.id!?.toString() + index}
+        renderItem={(results) => (
+          <List.Item
+            title={results.item?.name}
+            description={results.item?.artists?.map((item, ix) => {
+              return item;
+            })}
+            left={(props) => (
+              <Avatar.Image
+                size={20}
+                source={{
+                  uri: `${results.item?.imageUrl}`,
+                }}
+              />
+            )}
+            right={() => (
+              <IconButton
+                icon="cancel"
+                onPress={() => {
+                  setArray(array.filter((i) => i.name !== results.item.name));
+                }}
+              />
+            )}
+          />
+        )}
+      />
     </StyledColumnView>
   );
 };
